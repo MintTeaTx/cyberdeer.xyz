95b7a701466bc1b0084149c13410459264e65c9d
Sadie Davis <32.bit.mint.tin@gmail.com>
2022-08-01 15:41:38 -0500

    Revert "fixed links"
    This reverts commit b28995dbc5d97c73b1f21fb9dcc328eb42800a3d.

--- app.js
+++ app.js
@@ -8,29 +8,59 @@ const filedir = config.get('files.dir');
 const app = express();
 
 app.set('view engine', 'ejs');
-//app.use(express.favicon(__dirname, 'public/images/favicon.ico'));
+app.use(express.favicon(__dirname, 'public/images/favicon.ico'));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/index', function (req,res){
-
-try {
-  data = require('./views/pages/index.json');
-  console.log(data.data);
-  success(res,data.data);
-} catch(err) {
-  console.log( err);
-}
+app.get('/', function (req,res){
+   renderWithHeader(res, {filename :'/index'}, 'indexheader');
 });
 
+app.get('/files/*', (req,res) => {
 
+  let inpath = filedir + req.params[0];
+  let path = "/files/"+req.params[0];
 
-function success(res, data)
-{
-  res.json({success:true, data:data, err:''});
+    if(fs.statSync(inpath).isFile())
+    {
+      res.sendFile(inpath);
+      return;
+    }
+
+  let dirs = fs.readdirSync(inpath).filter(function(file) {
+    return fs.statSync(inpath+'/'+file).isDirectory();
+  });
+  let files =fs.readdirSync(inpath).filter(function(file) {
+    return fs.statSync(inpath+'/'+file).isFile();
+  });
+
+renderWithHeader(res, {filename :'/files', dirs:dirs, files:files, path:path}, 'stashheader');
+});
+
+app.all('*', (req, res) => {
+
+   const file = (req.params[0].charAt(req.params[0].length-1) === '/') ? req.params[0].slice(0, -1) : req.params[0]  ;
+   console.log(file);
+   fs.access('./views/pages'+file+'.ejs', fs.F_OK, (err)=> {
+      if (err) {
+         renderWithHeader(res, {filename :'/404'},'stdheader');
+         console.log("butts");
+      } else {
+        console.log("dicks");
+         renderWithHeader(res, {filename :''+file}, 'stdheader');
+      }
+
+   });
+});
+
+function renderWithHeader(res, array) {
+  console.log("stdheader"
+  );
+  renderWithHeader(res, array, 'stdheader')
 }
-function error(res,err)
-{
-  res.json({success:false, data:'', err:err});
+function renderWithHeader(res, array, header) {
+    array.header=header;
+    console.log(array.header);
+    res.render('templates/template', array);
 }
 app.listen(config.get('server.port'), function ()
 {

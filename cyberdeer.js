83b532dbb6dec59706810a18bcd518a624f47d91
MintTeaTx <32.bit.mint.tin@gmail.com>
2022-08-18 21:57:26 -0500

    poop
    yes, an intelligent commit message

---
+++ cyberdeer.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const favi = require('serve-favicon');
+const config = require('config');
+const filedir = config.get('files.dir');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(favi(__dirname+'/public/images/favicon.ico'));
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/', function (req,res){
+   renderWithHeader(res, {filename :'/index'}, 'indexheader');
+});
+
+
+
+app.get('/files/*', (req,res) => {
+
+  let inpath = filedir + req.params[0];
+  let path = "/files/"+req.params[0];
+
+try {
+  if(fs.statSync(inpath).isFile())
+  {
+    res.sendFile(inpath);
+    return;
+  }
+} catch (e) {
+  console.log(e);
+  show404(res);
+}
+
+  let dirs = fs.readdirSync(inpath).filter(function(file) {
+    return fs.statSync(inpath+'/'+file).isDirectory();
+  });
+  let files =fs.readdirSync(inpath).filter(function(file) {
+    return fs.statSync(inpath+'/'+file).isFile();
+  });
+
+renderWithHeader(res, {filename :'/files', dirs:dirs, files:files, path:path}, 'stashheader');
+});
+
+
+
+app.all('*', (req, res) => {
+
+   const file = (req.params[0].charAt(req.params[0].length-1) === '/') ? req.params[0].slice(0, -1) : req.params[0]  ;
+   console.log(file);
+   fs.access('./views/pages'+file+'.ejs', fs.F_OK, (err)=> {
+      if (err) {
+         show404(res);
+         console.log("butts");
+      } else {
+         renderWithHeader(res, {filename :''+file}, 'stdheader');
+      }
+
+   });
+});
+
+
+
+function show404(res)
+{
+  renderWithHeader(res, {filename :'/404'},'stdheader');
+
+}
+
+function renderWithHeader(res, array) {
+  console.log("stdheader"
+  );
+  renderWithHeader(res, array, 'stdheader')
+}
+function renderWithHeader(res, array, header) {
+    array.header=header;
+    console.log(array.header);
+    res.render('templates/template', array);
+}
+app.listen(config.get('server.port'), function ()
+{
+   console.log('listening on '+config.get('server.port'));
+});
--- public/js/cyberdeer.js
+++ public/js/cyberdeer.js
@@ -1,54 +1,73 @@
 let hidden = false;
-$(document).ready( function() {
 
-$("#x").on({
-  mouseenter: function() {
-    $(this).css("color", "red");
-  },
-  mouseleave: function() {
-    $(this).css("color", "white");
-  },
-  click: function() {
-    if (hidden) {
-      $(".terminal.body").show();
-    } else {
-      $(".terminal.body").hide();
+$(document).ready(function() {
+
+  $("#x").on({
+    mouseenter: function() {
+      $(this).css("color", "red");
+    },
+    mouseleave: function() {
+      $(this).css("color", "white");
+    },
+    click: function() {
+      if (hidden) {
+        $(".terminal.body").show();
+      } else {
+        $(".terminal.body").hide();
+      }
+      hidden = !hidden;
     }
-    hidden = !hidden;
-  }
-});
+  });
 
-$("#return").on({
-mouseenter: function() {
-  $(this).css("color", "green");
-},
-mouseleave: function() {
-  $(this).css("color", "white");
-},
-click: function() {
+  $("#return").on({
+    mouseenter: function() {
+      $(this).css("color", "green");
+    },
+    mouseleave: function() {
+      $(this).css("color", "white");
+    },
+    click: function() {
 
-  history.back();
+      history.back();
 
-}
-});
+    }
+  });
 
 
-$(".terminal>img").on({
-  click: function() {
-    console.log("Image clicked!");
-    console.log($(this).attr('src'));
-    $("#img01").attr('src', $(this).attr('src'));
-    $("#imageModal").css("display", "block");
-  }
-});
-$(".close").on({
-  click: function() {
-    $("#imageModal").css("display", "none");
-  }
-});
-$(".modal").on({
-  click: function(){
-    $("#imageModal").css("display", "none");
-  }
-})
+  $(".terminal>img").on({
+    click: function() {
+      console.log("Image clicked!");
+      console.log($(this).attr('src'));
+      $("#img01").attr('src', $(this).attr('src'));
+      $("#imageModal").css("display", "block");
+    }
+  });
+  $(".close").on({
+    click: function() {
+      $("#imageModal").css("display", "none");
+    }
+  });
+  $(".modal").on({
+    click: function() {
+      $("#imageModal").css("display", "none");
+    }
+  });
+  $("#treeview .parent").click(function (e) {
+      e.stopPropagation();
+      $(this).find(">ul").toggle("slow");
+      var caret = $(this).find(">ul>span");
+      if(caret.hasClass("caret-down")){
+        caret.removeClass("caret-down");
+      } else {
+        caret.addClass("caret-down");
+      }
+      if ($(this).hasClass("inactive"))
+          $(this).removeClass("inactive");
+      else
+          $(this).addClass("inactive");
+  });
+  $("li").click(function(e){
+    e.stopPropagation();
+    console.log($(this).text());
+  });
 });
